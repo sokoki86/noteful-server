909b88a5ab3cd0188459431a55756e5b5732f138
Jessica <jmbaskerville1217@gmail.com>
2021-01-19 14:15:09 -0500

    changed dependencies

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV || 'production',
     API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
     CLIENT_ORIGIN:process.env.CLIENT_ORIGIN,
     DATABASE_URL:process.env.DATABASE_URL,
