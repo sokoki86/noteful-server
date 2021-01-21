a7bcf1d4a8504041f598ac31b01111a11a8c2dea
Jessica <jmbaskerville1217@gmail.com>
2021-01-21 11:25:09 -0500

    console.log

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -2,7 +2,10 @@ const { NODE_ENV } = require('../config')
 
 module.exports = function errorHandler(error, req, res, next) {
     let response;
-    if (NODE_ENV === 'production') {
+    if (NODE_ENV === 'production') 
+    
+    {
+      console.log(error);
       response = { error: { message: "server error" } };
     } else {
       console.error(error);
