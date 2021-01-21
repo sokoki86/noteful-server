3c69a240220754f96cdc00b858fbaeb5846e9f22
Jessica <jmbaskerville1217@gmail.com>
2021-01-21 11:32:44 -0500

    added ssl

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
+const pg = require("pg"); pg.defaults.ssl = true;
 const knex = require('knex')
+
 
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
 })
+
 
 app.set('db', db)
 app.listen(PORT, () => {
