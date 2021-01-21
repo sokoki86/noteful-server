0cc31ef1a84cacbe38c18ee62e96fa9aa1e4c562
Jessica <jmbaskerville1217@gmail.com>
2021-01-21 11:39:36 -0500

    added require

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
+const pg = require("pg"); pg.defaults.ssl = true;
 
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": process.env.DATABASE_URL,
-}
+  }
